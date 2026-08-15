225d55f0ada75c4b48a6ee6c4c59a145cd604459
Show5 <shogo.maeda@gmail.com>
2026-08-15 17:57:06 +0900

    ランチャーファイルにモジュールの確認を追加

--- mac-start.command
+++ mac-start.command
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+
+if [ ! -d "server/node_modules" ]; then
+  echo "初回起動のため、依存パッケージをインストールしています..."
+  npm --prefix server install
+fi
+
 echo "サーバーを起動しています..."
 npm --prefix server run dev
--- win-start.bat
+++ win-start.bat
@@ -1,4 +1,10 @@
 @echo off
 cd /d "%~dp0"
-npm --prefix server run dev
+
+if not exist "server\node_modules" (
+  echo 初回起動のため、依存パッケージをインストールしています...
+  call npm --prefix server install
+)
+
+call npm --prefix server run dev
 pause
